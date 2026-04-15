665e0a558c0da8c5f4f66c7523d163b5ec840cf7
Andrew Lizarraga <Aflizarr@asu.edu>
2026-04-15 22:22:30 +0000

    Update DFA accept states to reflect correct transitions

--- app.py
+++ app.py
@@ -24,16 +24,16 @@ DFAS: Dict[str, Dict[str, object]] = {
       "description": "Contains at Least one 1 and an even number of 0s follows the last 1",
       "alphabet": {"0", "1"},
       "start_state": "q0",
-      "accept_states": {"q2"},
+      "accept_states": {"q1"},
       "transitions": {
-        ("q0", "0"): "q0",
-        ("q0", "1"): "q1",
-
-        ("q1", "0"): "q2",
-        ("q1", "1"): "q1",
-
-        ("q2", "0"): "q1",
-        ("q2", "1"): "q1",
+          ("q0", "0"): "q0",
+          ("q0", "1"): "q1",
+          
+          ("q1", "0"): "q2",
+          ("q1", "1"): "q1",
+          
+          ("q2", "0"): "q1",
+          ("q2", "1"): "q1",
       },
     },
     "dfa2": {
